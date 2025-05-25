8a2d2df54299e74ccb8c28f9f57c0f277bf5fbf6
Jonathan <jonathanjohn1133@gmail.com>
2025-05-25 19:45:46 +0530

    Changes

--- lib/features/paywall/ui/paywall_screen.dart
+++ lib/features/paywall/ui/paywall_screen.dart
@@ -1,3 +1,5 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:custom_rating_bar/custom_rating_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -137,27 +139,38 @@ class PaywallScreen extends StatelessWidget {
                   ],
                 ),
                 SizedBox(height: 20.sp),
-                Row(
-                  children: [
-                    Column(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(8.sp),
-
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8.sp),
-                            color: AppColors.lightGreyColor,
-                          ),
-                          child: Image.asset(
-                            "assets/images/live_classes.png",
-                            height: 48.sp,
-                            width: 48.sp,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: (16.0)),
+                  child: Row(
+                    children: [
+                      Expanded(child: fourOptionsWithImage(
+                          "assets/images/live_classes.png",
+                          "Live Classes Access")),
+                      Expanded(child: fourOptionsWithImage(
+                          "assets/images/ai_voice.png",
+                          "AI Voice Doubt Solver")),
+                      Expanded(
+                        child: fourOptionsWithImage(
+                            "assets/images/learning_materials.png",
+                            "Learning Materials"),
+                      ),
+                      Expanded(child: fourOptionsWithImage(
+                          "assets/images/gamified_ai.png",
+                          "Gamified AI Exercises")),
+                    ],
+                  ),
                 ),
+                SizedBox(height: 20.sp),
+                CarouselSlider(
+                  items: [
+                    carouselItem(
+                        "assets/images/john_anderson.png", "John Anderson",
+                        "Their attention to detail and creative design approach transformed our website into a visually stunning and highly functional platform. We’ve seen a 30% increase in traffic since the relaunch. I highly recommend them to anyone seeking professional web design services!")
+                  ],
+                  options: CarouselOptions(
+                      viewportFraction: 0.6,
+                      height: 0.2.sh,
+                      enlargeCenterPage: true),)
               ],
             ),
           ),
@@ -168,5 +181,55 @@ class PaywallScreen extends StatelessWidget {
 
   Widget webUi() {
     return Container();
+  }
+
+  Widget fourOptionsWithImage(String image, String title) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(10.sp),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.sp),
+            color: AppColors.lightGreyColor,
+          ),
+          child: Image.asset(
+            image,
+            height: 48.sp,
+            width: 48.sp,
+          ),
+        ),
+        SizedBox(height: 8.sp),
+        Text(title,
+          style: TextStyle(fontSize: 11.sp),
+          textAlign: TextAlign.center,)
+      ],
+    );
+  }
+
+  Widget carouselItem(String image, String name, String description) {
+    return Card(child: Padding(
+      padding:  EdgeInsets.all(16.0.sp),
+      child: Column(children: [
+        Row(children: [
+          CircleAvatar(
+            radius: 20.sp,
+            backgroundImage: AssetImage(image),
+          ),
+          SizedBox(width: 8.sp),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            Text(name, style: TextStyle(fontSize: 13.sp, fontWeight: FontWeight.w700),),
+            RatingBar.readOnly(filledIcon: Icons.star,
+                emptyIcon: Icons.star_border,
+                initialRating: 4,
+                filledColor: AppColors.purpleColor,
+
+                maxRating: 4
+            )
+          ],)
+        ],)
+      ],),
+    ), color: Colors.white,);
   }
 }
