576881d0b1917d05132f23adb03edfa27464c3b8
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2025-05-26 16:58:10 +0530

    UI Changes

--- lib/features/paywall/ui/paywall_screen.dart
+++ lib/features/paywall/ui/paywall_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:ostello/utility/app_colors.dart';
+import 'package:pixel_perfect/pixel_perfect.dart';
 
 import '../get_controllers/paywall_get_controller.dart';
 
@@ -15,17 +16,20 @@ class PaywallScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: OrientationBuilder(
-          builder: (context, orientation) {
-            if (orientation == Orientation.portrait) {
-              return mobileUi();
-            } else {
-              return webUi();
-            }
-          },
+    return PixelPerfect(
+      assetPath: "assets/images/pixel_perfect.png",
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: OrientationBuilder(
+            builder: (context, orientation) {
+              if (orientation == Orientation.portrait) {
+                return mobileUi();
+              } else {
+                return webUi();
+              }
+            },
+          ),
         ),
       ),
     );
@@ -34,24 +38,29 @@ class PaywallScreen extends StatelessWidget {
   Widget mobileUi() {
     return Column(
       children: [
+        SizedBox(height: 6.sp,),
         Stack(
           children: [
             Padding(
-              padding: EdgeInsets.only(left: 8.0.sp, top: 8.sp),
+              padding: EdgeInsets.only(left: 16.sp, top: 8.sp),
               child: CircleAvatar(
                 child: Icon(Icons.close, size: 20.sp, color: Colors.white),
                 radius: 15.r,
                 backgroundColor: AppColors.greyColor,
               ),
             ),
+
             Row(
               children: [
                 Expanded(
                   child: Text(
                     "The Ultimate Fluency\nMastery Course",
                     style: TextStyle(
-                      fontSize: 24.sp,
+                        fontSize: 22.sp,
                       fontWeight: FontWeight.w700,
+                        letterSpacing: -0.4,
+                        height: 1.3,
+                        wordSpacing: -3
                     ),
                     textAlign: TextAlign.center,
                   ),
@@ -64,9 +73,9 @@ class PaywallScreen extends StatelessWidget {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                SizedBox(height: 20.sp),
-                Image.asset("assets/images/giftbox.png", height: 73.sp),
-                SizedBox(height: 20.sp),
+                SizedBox(height: 1.sp),
+                Image.asset("assets/images/giftbox.png", height: 66.sp),
+                SizedBox(height: 4.sp),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -75,8 +84,9 @@ class PaywallScreen extends StatelessWidget {
                         Text(
                           "23",
                           style: TextStyle(
-                            fontSize: 32.sp,
+                            fontSize: 24.sp,
                             fontWeight: FontWeight.w700,
+                            height: 1.3,
                           ),
                         ),
                         Text(
@@ -100,8 +110,9 @@ class PaywallScreen extends StatelessWidget {
                         Text(
                           "55",
                           style: TextStyle(
-                            fontSize: 32.sp,
+                              fontSize: 24.sp,
                             fontWeight: FontWeight.w700,
+                              height: 1.3
                           ),
                         ),
                         Text(
@@ -125,8 +136,9 @@ class PaywallScreen extends StatelessWidget {
                         Text(
                           "39",
                           style: TextStyle(
-                            fontSize: 32.sp,
+                              fontSize: 24.sp,
                             fontWeight: FontWeight.w700,
+                              height: 1.3
                           ),
                         ),
                         Text(
@@ -142,7 +154,7 @@ class PaywallScreen extends StatelessWidget {
                 ),
                 SizedBox(height: 20.sp),
                 Padding(
-                  padding: EdgeInsets.symmetric(horizontal: (16.0)),
+                  padding: EdgeInsets.symmetric(horizontal: (40.0)),
                   child: Row(
                     children: [
                       Expanded(child: fourOptionsWithImage(
@@ -162,7 +174,7 @@ class PaywallScreen extends StatelessWidget {
                     ],
                   ),
                 ),
-                SizedBox(height: 20.sp),
+                SizedBox(height: 15.sp),
                 CarouselSlider(
                   items: [
                     Obx(
@@ -203,12 +215,12 @@ class PaywallScreen extends StatelessWidget {
                     ),
                   ],
                   options: CarouselOptions(
-                      viewportFraction: 0.7,
-                      height: 0.27.sh,
+                      viewportFraction: 0.6,
+                      height: 0.16.sh,
                       enlargeCenterPage: true, onPageChanged: (value, reason) {
                     getController.currentCarouselIndex.value = value.toDouble();
                   }),),
-                SizedBox(height: 10.sp),
+                SizedBox(height: 2.sp),
                 Obx(
                         () {
                       return DotsIndicator(
@@ -218,15 +230,15 @@ class PaywallScreen extends StatelessWidget {
                           color: AppColors.lessLightGreyColor,
                           // Inactive color
                           activeColor: AppColors.purpleColor,
-                          activeSize: Size(30.0.sp, 9.0.sp),
-                          size: Size(9.0.sp, 9.0.sp),
+                          activeSize: Size(30.0.sp, 7.0.sp),
+                          size: Size(7.0.sp, 7.0.sp),
                           activeShape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(5.0)),
                         ),
                       );
                     }
                 ),
-                SizedBox(height: 10.sp),
+                SizedBox(height: 2.sp),
                 Row
                   (
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -324,20 +336,20 @@ class PaywallScreen extends StatelessWidget {
     return Column(
       children: [
         Container(
-          padding: EdgeInsets.all(10.sp),
+          padding: EdgeInsets.all(2.sp),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(8.sp),
             color: AppColors.lightGreyColor,
           ),
           child: Image.asset(
             image,
-            height: 48.sp,
-            width: 48.sp,
+            height: 36.sp,
+            width: 40.sp,
           ),
         ),
         SizedBox(height: 8.sp),
         Text(title,
-          style: TextStyle(fontSize: 11.sp),
+          style: TextStyle(fontSize: 10.sp),
           textAlign: TextAlign.center,)
       ],
     );
@@ -352,12 +364,12 @@ class PaywallScreen extends StatelessWidget {
         gradient: LinearGradient(colors: [Color(0xFF292D32),Color(0xFF292D32), Colors.white, Colors.white],
             begin: Alignment.topLeft,
             end: Alignment.bottomRight),
-        borderRadius: BorderRadius.circular(10.r),
+        borderRadius: BorderRadius.circular(6.r),
       ),
       child: Container(
         decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.circular(10.r),
+          borderRadius: BorderRadius.circular(6.r),
           boxShadow: [
             BoxShadow(
               color: Colors.grey.withOpacity(0.2),
@@ -368,11 +380,11 @@ class PaywallScreen extends StatelessWidget {
           ],
         ),
         child: Padding(
-          padding: EdgeInsets.all(16.0.sp),
+          padding: EdgeInsets.symmetric(horizontal: 16.sp, vertical: 10.sp),
           child: Column(children: [
             Row(children: [
               CircleAvatar(
-                radius: 20.sp,
+                radius: 15.sp,
                 backgroundImage: AssetImage(image),
               ),
               SizedBox(width: 8.sp),
@@ -380,69 +392,67 @@ class PaywallScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(name, style: TextStyle(
-                      fontSize: 13.sp, fontWeight: FontWeight.w700),),
+                      fontSize: 9.5.sp, fontWeight: FontWeight.w700),),
                   RatingBar.readOnly(filledIcon: Icons.star,
                       emptyIcon: Icons.star_border,
                       initialRating: 4,
                       filledColor: AppColors.purpleColor,
                       maxRating: 4,
-                      size: 10.sp
+                      size: 9.sp
                   )
                 ],)
             ],),
             SizedBox(height: 8.sp),
-            Text(description, style: TextStyle(fontSize: 8.sp),)
+            Text(description,
+              style: TextStyle(fontSize: 6.5.sp, letterSpacing: -0.18),)
           ],),
         ),),
     ): Container(
-      margin: EdgeInsets.symmetric(horizontal: 8.sp, vertical: 8.sp),
-      padding: EdgeInsets.all(1.sp),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10.r),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.2),
-              spreadRadius: 3,
-              blurRadius: 8,
-              offset: const Offset(0, 0), // changes position of shadow
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.r),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 3,
+            blurRadius: 8,
+            offset: const Offset(0, 0), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.sp, vertical: 10.sp),
+        child: Column(children: [
+          Row(children: [
+            CircleAvatar(
+              radius: 15.sp,
+              backgroundImage: AssetImage(image),
             ),
-          ],
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(16.0.sp),
-          child: Column(children: [
-            Row(children: [
-              CircleAvatar(
-                radius: 20.sp,
-                backgroundImage: AssetImage(image),
-              ),
-              SizedBox(width: 8.sp),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(name, style: TextStyle(
-                      fontSize: 13.sp, fontWeight: FontWeight.w700),),
-                  RatingBar.readOnly(filledIcon: Icons.star,
-                      emptyIcon: Icons.star_border,
-                      initialRating: 4,
-                      filledColor: AppColors.purpleColor,
-                      maxRating: 4,
-                      size: 10.sp
-                  )
-                ],)
-            ],),
-            SizedBox(height: 8.sp),
-            Text(description, style: TextStyle(fontSize: 8.sp),)
+            SizedBox(width: 8.sp),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(name, style: TextStyle(
+                    fontSize: 9.5.sp, fontWeight: FontWeight.w700),),
+                RatingBar.readOnly(filledIcon: Icons.star,
+                    emptyIcon: Icons.star_border,
+                    initialRating: 4,
+                    filledColor: AppColors.purpleColor,
+                    maxRating: 4,
+                    size: 9.sp
+                )
+              ],)
           ],),
-        ),),
-    );
+          SizedBox(height: 8.sp),
+          Text(description,
+            style: TextStyle(fontSize: 6.5.sp, letterSpacing: -0.18),)
+        ],),
+      ),);
   }
 
   Widget fourOptionsButton(String title) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 16.sp, vertical: 8.sp),
+      margin: EdgeInsets.symmetric(horizontal: 32.sp, vertical: 8.sp),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(10.r),
@@ -456,7 +466,7 @@ class PaywallScreen extends StatelessWidget {
         ],
       ),
       child: ListTile(
-        title: Text(title, style: TextStyle(fontSize: 14.sp),),
+        title: Text(title, style: TextStyle(fontSize: 12.sp, letterSpacing: -0.3),),
         trailing: Icon(Icons.chevron_right, color: AppColors.purpleColor,),
       ),);
   }
