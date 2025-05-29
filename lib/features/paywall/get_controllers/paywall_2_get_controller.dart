c95acb57ac8a004e1ff90dbfb16837e37ea10cc3
Jonathan <jonathanjohn1133@gmail.com>
2025-05-29 12:44:39 +0530

    Carousel Slider Done

--- lib/features/paywall/get_controllers/paywall_2_get_controller.dart
+++ lib/features/paywall/get_controllers/paywall_2_get_controller.dart
@@ -1,2 +1,4 @@
 import 'package:get/get.dart';
-class Paywall2GetController extends GetxController {}
+class Paywall2GetController extends GetxController {
+  RxDouble currentCarouselIndex = 0.0.obs;
+}
--- lib/features/paywall/ui/paywall_2_screen.dart
+++ lib/features/paywall/ui/paywall_2_screen.dart
@@ -1,3 +1,5 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:custom_rating_bar/custom_rating_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -96,6 +98,52 @@ class Paywall2Screen extends StatelessWidget {
               ],
             ),
           ),
+          SizedBox(height: 8.sp),
+          CarouselSlider(
+            items: [
+              Obx(
+                      () {
+                    return carouselItem(
+                        "assets/images/john_anderson.png",
+                        "John Anderson",
+                        "Their attention to detail and creative design approach transformed our website into a visually stunning and highly functional platform. We’ve seen a 30% increase in traffic since the relaunch. I highly recommend them to anyone seeking professional web design services!",
+                        getController.currentCarouselIndex.value == 0);
+                  }
+              ),
+              Obx(
+                      () {
+                    return carouselItem(
+                        "assets/images/john_anderson.png",
+                        "John Anderson",
+                        "Their attention to detail and creative design approach transformed our website into a visually stunning and highly functional platform. We’ve seen a 30% increase in traffic since the relaunch. I highly recommend them to anyone seeking professional web design services!",
+                        getController.currentCarouselIndex.value == 1);
+                  }
+              ),
+              Obx(
+                      () {
+                    return carouselItem(
+                        "assets/images/john_anderson.png",
+                        "John Anderson",
+                        "Their attention to detail and creative design approach transformed our website into a visually stunning and highly functional platform. We’ve seen a 30% increase in traffic since the relaunch. I highly recommend them to anyone seeking professional web design services!",
+                        getController.currentCarouselIndex.value == 2);
+                  }
+              ),
+              Obx(
+                      () {
+                    return carouselItem(
+                        "assets/images/john_anderson.png",
+                        "John Anderson",
+                        "Their attention to detail and creative design approach transformed our website into a visually stunning and highly functional platform. We’ve seen a 30% increase in traffic since the relaunch. I highly recommend them to anyone seeking professional web design services!",
+                        getController.currentCarouselIndex.value == 3);
+                  }
+              ),
+            ],
+            options: CarouselOptions(
+                viewportFraction: 0.6,
+                height: 0.16.sh,
+                enlargeCenterPage: true, onPageChanged: (value, reason) {
+              getController.currentCarouselIndex.value = value.toDouble();
+            }),),
         ],
       ),
     );
@@ -127,4 +175,100 @@ class Paywall2Screen extends StatelessWidget {
       ],
     );
   }
+}
+
+
+Widget carouselItem(String image, String name, String description,
+    bool border) {
+  return border? Container(
+    margin: EdgeInsets.symmetric(horizontal: 8.sp, vertical: 8.sp),
+    padding: EdgeInsets.all(1.sp),
+    decoration: BoxDecoration(
+      gradient: LinearGradient(colors: [Color(0xFF292D32),Color(0xFF292D32), Colors.white, Colors.white],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight),
+      borderRadius: BorderRadius.circular(6.r),
+    ),
+    child: Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(6.r),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 3,
+            blurRadius: 8,
+            offset: const Offset(0, 0), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 13.sp, vertical: 13.sp),
+        child: Column(children: [
+          Row(children: [
+            CircleAvatar(
+              radius: 15.sp,
+              backgroundImage: AssetImage(image),
+            ),
+            SizedBox(width: 8.sp),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(name, style: TextStyle(
+                    fontSize: 9.5.sp, fontWeight: FontWeight.w700),),
+                RatingBar.readOnly(filledIcon: Icons.star,
+                    emptyIcon: Icons.star_border,
+                    initialRating: 4,
+                    filledColor: AppColors.purpleColor,
+                    maxRating: 4,
+                    size: 9.sp
+                )
+              ],)
+          ],),
+          SizedBox(height: 8.sp),
+          Text(description,
+            style: TextStyle(fontSize: 6.5.sp, letterSpacing: -0.7, wordSpacing: -0.5),)
+        ],),
+      ),),
+  ): Container(
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(10.r),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.2),
+          spreadRadius: 3,
+          blurRadius: 8,
+          offset: const Offset(0, 0), // changes position of shadow
+        ),
+      ],
+    ),
+    child: Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.sp, vertical: 10.sp),
+      child: Column(children: [
+        Row(children: [
+          CircleAvatar(
+            radius: 15.sp,
+            backgroundImage: AssetImage(image),
+          ),
+          SizedBox(width: 8.sp),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(name, style: TextStyle(
+                  fontSize: 9.5.sp, fontWeight: FontWeight.w700),),
+              RatingBar.readOnly(filledIcon: Icons.star,
+                  emptyIcon: Icons.star_border,
+                  initialRating: 4,
+                  filledColor: AppColors.purpleColor,
+                  maxRating: 4,
+                  size: 9.sp
+              )
+            ],)
+        ],),
+        SizedBox(height: 8.sp),
+        Text(description,
+          style: TextStyle(fontSize: 6.5.sp, letterSpacing: -1.8),)
+      ],),
+    ),);
 }
